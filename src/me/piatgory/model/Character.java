cf3c637bf22215fea05b6a63fc26493bb10c8fce
gregpiatperso <gregoire.piat@outlook.fr>
2015-12-10 10:02:53 +0100

    Adding attributes and constructor to Character class

--- src/me/piatgory/model/Character.java
+++ src/me/piatgory/model/Character.java
@@ -4,4 +4,22 @@ package me.piatgory.model;
  * Created by Grégoire on 10/12/2015.
  */
 public class Character {
+
+    private String name;
+    private double health;
+    private double stamina; //Résistance
+    private double power;
+    private double accurancy;
+    private int weight;
+
+
+
+    public Character(String name, double health, double stamina, double power, double accurancy) {
+        this.name = name;
+        this.health = health;
+        this.stamina = stamina;
+        this.power = power;
+        this.accurancy = accurancy;
+        this.weight = weight;
+    }
 }
