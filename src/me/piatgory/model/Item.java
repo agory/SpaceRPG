0c1f3437ee4a0768dcb1e0833aa22523b793a738
agory <gory.alexandre02@gmail.com>
2015-12-10 10:02:54 +0100

    Add attribut class Itam

--- src/me/piatgory/model/Item.java
+++ src/me/piatgory/model/Item.java
@@ -4,4 +4,27 @@ package me.piatgory.model;
  * Created by Grégoire on 10/12/2015.
  */
 public class Item {
+    private String nom;
+    private int poid;
+
+    public Item(int poid, String nom) {
+        this.poid = poid;
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPoid() {
+        return poid;
+    }
+
+    public void setPoid(int poid) {
+        this.poid = poid;
+    }
 }
