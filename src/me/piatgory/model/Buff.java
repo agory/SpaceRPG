5d4ae42b6045650881114213f6a41de087b0ee53
agory <gory.alexandre02@gmail.com>
2015-12-17 12:01:06 +0100

    Update Stat enum

--- src/me/piatgory/model/Buff.java
+++ src/me/piatgory/model/Buff.java
@@ -4,6 +4,18 @@ package me.piatgory.model;
  * Created by Alexandre Gory on 17/12/2015.
  */
 public class Buff {
+    private String name;
     private Stats stats;
     private int healthEffect;
+
+    public Buff(String name, Stats stats, int healthEffect) {
+        this.name = name;
+        this.stats = stats;
+        this.healthEffect = healthEffect;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
