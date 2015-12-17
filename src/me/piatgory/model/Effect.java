5240f941b2e63125ce5c7f46622d8076c3fea497
gregpiatperso <gregoire.piat@outlook.fr>
2015-12-17 10:47:18 +0100

    Adding Effect.java

---
+++ src/me/piatgory/model/Effect.java
@@ -0,0 +1,15 @@
+package me.piatgory.model;
+
+/**
+ * Created by Grégoire on 17/12/2015.
+ */
+public class Effect {
+    private Stats stat;
+    private int value;
+    private int permanent;
+
+    public Effect(){
+
+    }
+
+}
