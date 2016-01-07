622dc13d712a97dc35f024288c32a8aa8e650550
Agory <gory.alexandre02@gmail.com>
2016-01-07 11:01:53 +0100

    Stats update

--- src/me/piatgory/model/Stats.java
+++ src/me/piatgory/model/Stats.java
@@ -13,9 +13,26 @@ public class Stats {
             stats.put(stat.getName(), stat.getValue());
         }
     }
-    public void merge(){
-
+    public void merge(Stats stats){
+        for (String key : stats.getStats().keySet()) {
+            if(this.stats.containsKey(key)){
+                this.stats.replace(key,this.stats.get(key) + stats.getStats().get(key));
+            } else {
+                this.stats.put(key,stats.getStats().get(key));
+            }
+        }
     }
 
+    public HashMap<String, Integer> getStats() {
+        return stats;
+    }
 
+    @Override
+    public String toString() {
+        String message = "Stats :";
+        for (String key : stats.keySet()) {
+            message += " - " + key + " : " + stats.get(key);
+        }
+        return message;
+    }
 }
